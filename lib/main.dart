b0163d19da19341ba6cf0265b6d15054dd7d2cf2
med363 <blibechmedamine@gmail.com>
2022-07-07 14:26:28 +0100

    simplelist

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("various ListView"),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title:Text("item1")
+              ,
+            )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item2")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item3")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item4")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item5")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item6")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item7")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item8")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item9")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item10")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item11")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item12")
+                ,
+              )
+          ),
+          Card(
+              child: ListTile(
+                title:Text("item13")
+                ,
+              )
+          ),
+
+        ],
+        scrollDirection: Axis.vertical,
+      ),
+    );
+  }
+}
